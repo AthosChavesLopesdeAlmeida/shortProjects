b82425582cc639172738bddff47315fcc9d3e391
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-09-25 18:22:08 -0300

    25/09/2025

---
+++ imageCarousel/index.js
@@ -0,0 +1,86 @@
+const carousel = document.getElementById('carousel');
+const carouselContainer = document.getElementById('imageCarousel');
+const controlsContainer = document.getElementById('controls');
+const nextBtn = document.getElementById('nextBtn');
+const prevBtn = document.getElementById('prevBtn');
+const sermon = document.getElementById('sermon');
+const crucifixion = document.getElementById('crucifixion');
+const supper = document.getElementById('supper');
+const indexText = document.getElementById('index')
+
+let images = [crucifixion, supper, sermon];
+let currentIndex = 0;
+images[0].classList.add('shown');
+images[1].classList.add('hidden');
+images[2].classList.add('hidden');
+
+function prevImg() {
+  if (currentIndex === 0) {
+    indexText.innerHTML = '3/3';
+    currentIndex = 2;
+    
+    images[2].classList.add('shown');
+    images[2].classList.remove('hidden');
+    images[0].classList.add('hidden');
+    images[0].classList.remove('shown');
+  } 
+  else if (currentIndex === 1) {
+    indexText.innerHTML = '1/3'
+    currentIndex = 0;
+    
+    images[0].classList.add('shown');
+    images[0].classList.remove('hidden');
+    images[1].classList.add('hidden');
+    images[1].classList.remove('shown');
+  } 
+  else {
+    indexText.innerHTML = '2/3'
+    currentIndex = 1;
+
+    images[1].classList.add('shown');
+    images[1].classList.remove('hidden');
+    images[2].classList.add('hidden');
+    images[2].classList.remove('shown');
+  }
+
+
+}
+
+function nextImg() {
+   if (currentIndex === 0) {
+    indexText.innerHTML = '2/3';
+    currentIndex = 1;
+
+    images[1].classList.add('shown');
+    images[1].classList.remove('hidden');
+    images[0].classList.add('hidden');
+    images[0].classList.remove('shown');
+  } 
+  else if (currentIndex === 1) {
+    indexText.innerHTML = '3/3'
+    currentIndex = 2;
+
+    images[2].classList.add('shown');
+    images[2].classList.remove('hidden');
+    images[1].classList.add('hidden');
+    images[1].classList.remove('shown');
+  } 
+  else {
+    indexText.innerHTML = '1/3'
+    currentIndex = 0;
+
+    images[0].classList.add('shown');
+    images[0].classList.remove('hidden');
+    images[2].classList.add('hidden');
+    images[2].classList.remove('shown');
+  }
+}
+
+nextBtn.addEventListener('click', () => {
+  nextImg();
+});
+
+prevBtn.addEventListener('click', () => {
+  prevImg()
+})
+
