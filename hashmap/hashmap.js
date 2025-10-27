05a37caf0c7953d02603320fcf30d3f99e73bc41
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-10-27 11:45:13 -0300

    27/10/2025

--- hashmap/hashmap.js
+++ hashmap/hashmap.js
@@ -1,8 +1,9 @@
 class HashMap {
   constructor() {
     this.loadFactor = 0.75;
-    this.capacity = 16;
-    this.storage = new Array(this.capacity);
+    this.capacity = 16; // Capacidade inicial de buckets
+    this.storage = new Array(this.capacity); // Cria um array de buckets com o tamanho da capacidade
+    this.size = 0; // número de itens armazenados
   }
 
   // Hashing define o índice para uma chave baseado no cálculo do código hash
@@ -46,11 +47,35 @@ class HashMap {
         }
       }
 
-      // Se a chave não foi encontrada, adiciona um novo par [chave, valor]
+      // Se a chave não foi encontrada, só então insere um novo par [chave, valor] nesse bucket
       if (inserted === false) {
         this.storage[index].push([key, value]);
+        this.size++;
+
+        if (this.size / this.capacity > this.loadFactor) {
+          this.resize(this.capacity * 2);  // <-- Isso é o parametro newCapacity
+        }
       }
     }
+ }
+
+ resize(newCapacity) {
+  const oldStorage = this.storage;
+  this.capacity =  newCapacity; // <-- ¹
+  this.storage  = new Array(this.capacity);
+
+  for (let i = 0; i < oldStorage.length; i++) {
+    const bucket = oldStorage[i];
+    if (!bucket) continue;
+    
+    for (let z = 0; z < bucket.length; z++) {
+      const [key, value] = bucket[z];
+      const index = this.hash(key); // Usa a capacidade atualizada¹
+      if(!this.storage[index]) this.storage[index] = []; // Se não existe um bucket naquele índice, cria um vazio
+      this.storage[index].push([key, value]); // Reinsere o par [chave, valor] no novo storage com o novo índice (com novo hash e nova capacidade)
+    } 
+    return "resize completo";
+  }
  }
 
  get(key) {
@@ -97,6 +122,7 @@ class HashMap {
     for (let i = 0; i < bucket.length; i++) {
       if (bucket[i][0] === key) {
         bucket.splice(i, 1);
+        this.size--;
         if (bucket.length === 0) this.storage[index] = undefined; // Limpa slot vazio
         return true;
       } 
@@ -126,62 +152,82 @@ class HashMap {
     const bucket = this.storage[i] 
     if (!bucket) continue; 
     bucket[i] = undefined
-    bucket.length = 0
+    bucket.length = 0 // Limpa cada bucket de acordo com a iteração do laço
   }
   
   return 'HashMap limpo com sucesso!';
  }
 
- // Arrumar (Retorna os pares completos [o que não é necessariamente ruim] e não as chaves apenas)
+
  keys() {
   const keysArray = [];
   
   for (let i = 0; i < this.storage.length; i++) {
     const bucket = this.storage[i];
     if (!bucket) continue;
-    
-    bucket.forEach(key => {
-      keysArray.push(key)
-    });
-  }
 
+    for (let z = 0; z < bucket.length; z++) {
+      keysArray.push(bucket[z][0]) // Pega a chave (índice 0) de cada par [chave, valor] 
+    }
+  }
   return keysArray;
+ }
+
+ values() {
+  const valuesArray = [];
+  
+  for (let i = 0; i < this.storage.length; i++) {
+    const bucket = this.storage[i];
+    if (!bucket) continue;
+
+    for (let z = 0; z < bucket.length; z++) {
+      valuesArray.push(bucket[z][1]) // Pega o valor (índice 1) de cada par [chave, valor] 
+    }
+  }
+  return valuesArray;
+ }
+
+ entries() {
+  const allArray = [];
+  
+  for (let i = 0; i < this.storage.length; i++) {
+    const bucket = this.storage[i];
+    if (!bucket) continue;
+
+    for (let z = 0; z < bucket.length; z++) {
+      allArray.push(bucket[z]) // Pega o valor (índice 1) de cada par [chave, valor] 
+    }
+  }
+  return allArray;
  }
 }
 
 let hm = new HashMap();
 
-// // Sets iniciais
-// hm.set("Brasil", "Brasília");
-// hm.set("Argentina", "Buenos Aires");
-// hm.set("Colômbia", "Bogotá");
-
-// // Retorna o valor 'Brasília'
-// console.log(hm.get("Brasil"));
-
-// // Retorna false
-// console.log(hm.has("Peru"));
-
-// // Retorna 3
-// console.log(hm.length());
-
-// // Limpa o HashMap
-// console.log(hm.clear())
-
-// // Retorna 0
-// console.log(hm.length());
-
-// Testes após limpar
 hm.set("Paquistão", "Islamabad");
 hm.set("Índia", "Nova Deli");
 hm.set("Bangladesh", "Daca");
 hm.set("Cazaquistão", "Nursultan");
+hm.set("Uzbequistão", "Tashkent");
+hm.set("Turquia", "Ancara");
+hm.set("Georgia", "Tbilisi");
+hm.set("Armênia", "Ierevan");
+hm.set("Rússia", "Moscou");
+hm.set("Bielorússia", "Minsk");
+hm.set("Ucrânia", "Kiev");
+hm.set("Moldávia", "Chisinau");
+hm.set("Afeganistão", "Cabul");
+hm.set("Irã", "Teerã");
+hm.set("Iraque", "Bagdá");
+hm.set("Síria", "Damasco");
+hm.set("Líbano", "Beirute");
 
-console.log(hm.length())
+console.log(hm.entries());
 
-// Retorna 'Islamabad'
-console.log(hm.get("Paquistão"));
 
-// Retorna true
-console.log(hm.has("Índia"));
-console.log(hm.keys())
+
+
+
+
+
+
